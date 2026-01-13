dcca6a6f176761103cae95891b12bd5c87c6cb48
andrei <protoandrei@protonmail.com>
2026-01-13 17:48:24 +0100

    Add `init` command to clone the Odoo repository

--- dv_launcher/main.py
+++ dv_launcher/main.py
@@ -1,6 +1,6 @@
 import typer
 
-from dv_launcher.cli import config, db, deploy
+from dv_launcher.cli import config, db, deploy, init
 from dv_launcher.cli.version import version
 
 app = typer.Typer(
@@ -12,7 +12,8 @@ app = typer.Typer(
 app.add_typer(config.app, name="config")
 app.add_typer(db.app, name="db")
 
-# Add version as a direct command
+# Add direct commands
+app.command(name="init")(init.init)
 app.command(name="version")(version)
 
 # Register the main deploy command as the default callback
